e4826aac4ff1cc2e449f32f93814f498ebc824cd
rk9155 <47426714+rk9155@users.noreply.github.com>
2021-12-25 21:59:03 +0530

    Add Firebase to Project, Add-Update-Delete from Cart

--- src/App.js
+++ src/App.js
@@ -1,44 +1,75 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 class App extends React.Component {
   constructor(){
         super();
         this.state = {
-            products: [
-                {
-                    price: 99,
-                    title:'Phone',
-                    qty: 1,
-                    img :'https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg',
-                    id: 1
-                },
-                {
-                    price: 88,
-                    title:'Tablet',
-                    qty: 1,
-                    img :'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-202109-gallery-1?wid=2822&hei=2400&fmt=jpeg&qlt=80&.v=1629916218000',
-                    id: 2
-                },
-                {
-                    price: 77,
-                    title:'Laptop',
-                    qty: 1,
-                    img :'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png',
-                    id: 3
-                },
-                {
-                    price: 66,
-                    title:'Smart watch',
-                    qty: 1,
-                    img :'https://i.gadgets360cdn.com/products/wearables/large/1551968932_832_apple_watch-series-4-gps-cellular-smartwatch.jpg',
-                    id: 4
-                }
-            ]
+            // products: [
+            //     {
+            //         price: 99,
+            //         title:'Phone',
+            //         qty: 1,
+            //         img :'https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg',
+            //         id: 1
+            //     },
+            //     {
+            //         price: 88,
+            //         title:'Tablet',
+            //         qty: 1,
+            //         img :'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-202109-gallery-1?wid=2822&hei=2400&fmt=jpeg&qlt=80&.v=1629916218000',
+            //         id: 2
+            //     },
+            //     {
+            //         price: 77,
+            //         title:'Laptop',
+            //         qty: 1,
+            //         img :'https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png',
+            //         id: 3
+            //     },
+            //     {
+            //         price: 66,
+            //         title:'Smart watch',
+            //         qty: 1,
+            //         img :'https://i.gadgets360cdn.com/products/wearables/large/1551968932_832_apple_watch-series-4-gps-cellular-smartwatch.jpg',
+            //         id: 4
+            //     }
+            // ]
+
+            products: [],
+            loading: true
             
         }
     }
+
+        componentDidMount() {
+            firebase
+                .firestore()
+                    .collection('products')
+                        .get()
+                            .then((snapshot) =>{
+                                console.log(snapshot);
+
+                                snapshot.docs.map((doc) => {
+                                    console.log(doc.data());
+                                });
+
+                                const products = snapshot.docs.map((doc) => {
+
+                                    const data = doc.data();
+                                    data['id'] = doc.id;
+                                    return data;
+                                });
+
+                                this.setState({
+                                    products: products,
+                                    loading: false
+                                });
+                            });
+        }
 
         handleIncreaseQuantity = (product) =>{
             const { products } = this.state;
@@ -62,7 +93,7 @@ class App extends React.Component {
 
             this.setState(
                 {
-                    products: products
+                    products: products,
                 });
         }
 
@@ -97,11 +128,24 @@ class App extends React.Component {
           return total;
         }
 
+        addProduct = () => {
+            firebase.firestore().collection('products')
+            .add({
+                price: 66,
+                title:'Smart watch',
+                qty: 1,
+                img :'https://i.gadgets360cdn.com/products/wearables/large/1551968932_832_apple_watch-series-4-gps-cellular-smartwatch.jpg'
+            }).then((docRef) => {
+                console.log('Product Added');
+            })
+        }
+
   render(){
-  const {products} = this.state;
+  const {products, loading} = this.state;
   return (
     <div className="App">
       <Navbar count = {this.getCartCount()} />
+      <button onClick={this.addProduct}>Add Product</button>
       <Cart 
 
         products = {products}
@@ -109,6 +153,8 @@ class App extends React.Component {
         OnDecreaseQuantity = {this.handleDecreaseQuantity}
         OnDeleteQuantity = {this.handleDeleteProduct}
       />
+
+      {loading && <h1> Loading Products....</h1>}
 
       <div>TOTAL : {this.getCartTotal()} </div>
     </div>);
--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDW1vSbf7QYpb7T9Mky6cEV_KRUosOsQbA",
+  authDomain: "cart-c668b.firebaseapp.com",
+  projectId: "cart-c668b",
+  storageBucket: "cart-c668b.appspot.com",
+  messagingSenderId: "1030974735334",
+  appId: "1:1030974735334:web:a6785079e0adabc22ccc27",
+  measurementId: "G-BXYGYXY9EJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render( <App />, document.getElementById('root'));
